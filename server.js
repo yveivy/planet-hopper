03761b1fc850d22a2f72f4e735b80ab8783b6914
Valais Girl <Yevette@Hal-71.local>
2023-05-14 20:45:47 -0500

    server and sql connection

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: '',
+    database: 'gameitems_db'
+  },
+  console.log(`Connected to the gameitems_db database.`)
+);
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
