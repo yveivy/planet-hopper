4601a45a012bb35a3d110d7ebafaa13829e85fae
Edward Wells <edward.wells87@gmail.com>
2023-05-16 07:50:55 -0500

    boundaries are in their places

--- public/js/gameboard.js
+++ public/js/gameboard.js
@@ -11,8 +11,8 @@ canvas.height = 576;
 
 colArrMap = []
 
-for (let i = 0; i < colArr.length; i += 60) { 
-    colArrMap.push(colArr.slice(i, 60 +i)) //slicing the main collisions array into rows of 60(amount of tiles per row)
+for (let i = 0; i < colArr.length; i += 60) {
+    colArrMap.push(colArr.slice(i, 60 + i)) //slicing the main collisions array into rows of 60(amount of tiles per row)
 
 }
 
@@ -21,16 +21,16 @@ for (let i = 0; i < colArr.length; i += 60) {
 class Boundary {
     static width = 64;  //the number represents the pixel square size*the zoom level 
     static height = 64;
-    constructor({position}) {
+    constructor({ position }) {
         this.position = position
         this.width = 64   //pre positioning boundaries based on the 400percent zoom map
         this.height = 64
     }
 
-draw() {
-    ctx.fillStyle = 'red'
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
-}
+    draw() {
+        ctx.fillStyle = 'rgba(255, 0, 0, 0.0)' //this makes the boundaries invisible  
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
 }
 const boundaries = []
 const offsetBoundary = {  //offsetting boundary to match where map is positioned
@@ -40,13 +40,14 @@ const offsetBoundary = {  //offsetting boundary to match where map is positioned
 
 colArrMap.forEach((row, j) => {
     row.forEach((symbol, k) => {
-        if(symbol === 4750)
-        boundaries.push(
-            new Boundary({
-                position: {
-            x: k * Boundary.width + offsetBoundary.x,   // this is just the width of the map squares with the image zoom
-            y: j * Boundary.height + offsetBoundary.y
-        }}))
+        if (symbol === 4750)
+            boundaries.push(
+                new Boundary({
+                    position: {
+                        x: k * Boundary.width + offsetBoundary.x,   // this is just the width of the map squares with the image zoom
+                        y: j * Boundary.height + offsetBoundary.y
+                    }
+                }))
     })
 });
 
@@ -73,7 +74,7 @@ class Sprite {
         position,
         velocity,
         image,
-        frames = {max: 1},
+        frames = { max: 1 },
     }) {
         this.frames = frames
 
@@ -81,29 +82,29 @@ class Sprite {
         this.image = image
         this.image.onload = () => {
             this.width = this.image.width / this.frames.max
-            this.height = this.image.height 
+            this.height = this.image.height
         }
-        
+
 
     }
 
     draw() {
-    
-    ctx.drawImage(
-        this.image,
-        0, //x coord for crop
-        0, //y crop 
-        this.image.width / this.frames.max, //crop from left to right of img
-        this.image.height, //full length of crop img
-        this.position.x,
-        this.position.y,
-        this.image.width / this.frames.max,
-        this.image.height //last for arguments are the actual positioning of sprite on screen
-    )//sprite guy position may need slight adjust later
+
+        ctx.drawImage(
+            this.image,
+            0, //x coord for crop
+            0, //y crop 
+            this.image.width / this.frames.max, //crop from left to right of img
+            this.image.height, //full length of crop img
+            this.position.x,
+            this.position.y,
+            this.image.width / this.frames.max,
+            this.image.height //last for arguments are the actual positioning of sprite on screen
+        )//sprite guy position may need slight adjust later
     }
 }
 
-        
+
 const newDude = new Sprite({
     position: {
         x: canvas.width / 2 - 256 / 4 / 2, // these two should perfectly center this.image on canvas if he is 256/64,
@@ -146,7 +147,13 @@ const testBoundary = new Boundary({
     }
 })
 
-const moveables = [background, testBoundary]
+const moveables = [background, ...boundaries]
+//below function replaced my giant if statement that covered how the collision was narrowed down to the collision array objects positions on the map
+function boxCollision({ rectangle1, rectangle2 }) {
+    return (
+        rectangle1.position.x + rectangle1.width >= rectangle2.position.x && rectangle1.position.x <= rectangle2.position.x + rectangle2.width && rectangle1.position.y <= rectangle2.position.y + rectangle2.height && rectangle1.position.y + rectangle1.height >= rectangle2.position.y
+    )
+}
 
 //animation loop
 
@@ -154,38 +161,123 @@ function animate() {
 
     window.requestAnimationFrame(animate) //infinite loop for the animation 
     background.draw()
-    // boundaries.forEach(boundary => {
-    //     boundary.draw()
-    // })
-testBoundary.draw()
-newDude.draw()
-    
+    boundaries.forEach(boundary => {
+        boundary.draw()
 
-    //if for collision detection. i am comparing dude's current position to the absolute position of each collision block  
-if (newDude.position.x + newDude.width >= testBoundary.position.x && newDude.position.x <= testBoundary.position.x + testBoundary.width&& newDude.position.y <= testBoundary.position.y + testBoundary.height && newDude.position.y + newDude.height >=testBoundary.position.y){
-    console.log('colliding')
+    })
+
+    newDude.draw()
+
+
+
+
+    // if(spriteGuy.position.x + spriteGuy.width)
+
+    //if statement for key actions to follow. !!!!! somewhere in movement there is a glitch that freezes right side obstacle interaction  
+
+
+
+    let moving = false
+    if (keys.w.pressed && lastkey === 'w') {
+        moving = true
+        for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+            if (
+                boxCollision({
+                    rectangle1: newDude,
+                    rectangle2: {
+                        ...boundary,
+                        position: {
+                            x: boundary.position.x,
+                            y: boundary.position.y + 3 //'predicting' that a boundary is about to get hit by using a 3pixel buffer
+                        }
+                    }
+                })
+            ) {
+                console.log('collision')
+                moving = false   //making a break and movement stop so the map quits moving and js quits detecting for other boundaries because it keeps giving a false reading 
+                break
+            }
+        }
+
+
+        if (moving)
+            moveables.forEach(moveable => {
+                moveable.position.y += 3
+            })
+    }else if (keys.a.pressed && lastkey === 'a') {
+        moving = true
+        for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+            if (
+                boxCollision({
+                    rectangle1: newDude,
+                    rectangle2: {
+                        ...boundary,
+                        position: {
+                            x: boundary.position.x + 3,
+                            y: boundary.position.y
+                        }
+                    }
+                })
+            ) {
+                console.log('collision')
+                moving = false
+                break
+            }
+        }if (moving)
+            moveables.forEach(moveable => { moveable.position.x += 3 })
+    }else if (keys.s.pressed && lastkey === 's') {
+        moving = true
+        for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+            if (
+                boxCollision({
+                    rectangle1: newDude,
+                    rectangle2: {
+                        ...boundary,
+                        position: {
+                            x: boundary.position.x,
+                            y: boundary.position.y - 3
+                        }
+                    }
+                })
+            ) {
+                console.log('collision')
+                moving = false
+                break
+            }
+        } if (moving)
+            moveables.forEach(moveable => { moveable.position.y -= 2 })
+    }else if (keys.d.pressed && lastkey === 'd') {
+        moving = true
+        for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+            if (
+                boxCollision({
+                    rectangle1: newDude,
+                    rectangle2: {
+                        ...boundary,
+                        position: {
+                            x: boundary.position.x - 3,
+                            y: boundary.position.y
+                        }
+                    }
+                })
+            ) {
+                console.log('collision')
+                moving = false
+                break
+            }
+        }if (moving)
+        moveables.forEach(moveable => { moveable.position.x -= 3 })
+    }
 }
 
-   // if(spriteGuy.position.x + spriteGuy.width)
-    
-    //if statement for key actions 
-    if (keys.w.pressed && lastkey === 'w'){ 
-        moveables.forEach(moveable => {moveable.position.y += 3})
-    }
-    else if (keys.a.pressed && lastkey === 'a'){ 
-        moveables.forEach(moveable => {moveable.position.x += 3})
-    } 
-    else if (keys.s.pressed && lastkey === 's'){ 
-        moveables.forEach(moveable => {moveable.position.y -= 3})
-    } 
-    else if (keys.d.pressed && lastkey === 'd'){ 
-        moveables.forEach(moveable => {moveable.position.x -= 3})
-    }
-}
 //listen for the keydown 
 let lastkey = ''
 window.addEventListener('keydown', (e) => {
-   // console.log('e.key') //logging the keydown event
+    // console.log('e.key') //logging the keydown event
     switch (e.key) {  //switch case for keydown 
         case 'w':
             keys.w.pressed = true
@@ -204,7 +296,7 @@ window.addEventListener('keydown', (e) => {
             lastkey = 'd'
             break
     }
-    
+
 })
 
 
@@ -212,7 +304,7 @@ window.addEventListener('keydown', (e) => {
 animate()
 
 window.addEventListener('keyup', (e) => {
-     //logging the keyup event
+    //logging the keyup event
     switch (e.key) {  //switch case for keyup 
         case 'w':
             keys.w.pressed = false
