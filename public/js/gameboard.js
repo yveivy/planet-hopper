fc11043a58aa0b4d21b9c5e26c7534bdb469c70c
Edward Wells <edward.wells87@gmail.com>
2023-05-17 08:36:50 -0500

    set interactions zones so each zone is now capable of triggering an individual interaction

--- public/js/gameboard.js
+++ public/js/gameboard.js
@@ -5,6 +5,20 @@ const backgroundImage = new Image();
 backgroundImage.src = './images/MapProjectZoomedPng.png';
 canvas.width = 1024;
 canvas.height = 576;
+
+gsap.to("#myImage", { duration: 1, opacity: 1 });
+
+
+setTimeout(function() {
+  gsap.to("#myImage", { duration: 1, opacity: 0 });
+}, 5000);
+
+gsap.to('#overlapping', {
+    opacity: 1,
+    repeat: 7,
+    yoyo: true,
+})
+
 colArrMap = []
 for (let i = 0; i < colArr.length; i += 60) {
     colArrMap.push(colArr.slice(i, 60 + i)) //slicing the main collisions array into rows of 60(amount of tiles per row)
@@ -20,29 +34,97 @@ colArrMap.forEach((row, j) => {
             boundaries.push(
                 new Boundary({
                     position: {
-                        x: k * Boundary.width + offsetBoundary.x,   // this is just the width of the map squares with the image zoom
+                        value: this.symbol, 
+                        x: k * Boundary.width + offsetBoundary.x,  
                         y: j * Boundary.height + offsetBoundary.y
                     }
                 }))
     })
 });
+const interactionZoneMap = []
+for (let i = 0; i < interactionArrData.length; i += 60) {
+    interactionZoneMap
+.push(interactionArrData.slice(i, 60 + i)) 
+}
+console.log(interactionZoneMap) //made an array of 60 arrays containing the interactions tiles
+
+const interactions = [];
+interactionZoneMap.forEach((row, j) => {
+    row.forEach((symbol, k) => {
+        if (symbol === 4750) {
+            interactions.push(
+                new Boundary({
+                    position: {
+                        value: symbol,
+                        x: k * Boundary.width + offsetBoundary.x,  
+                        y: j * Boundary.height + offsetBoundary.y
+                    }
+                })
+            );
+        } else if (symbol === 2020) {
+            interactions.push(
+                new Boundary({
+                    position: {
+                        value: symbol, 
+                        x: k * Boundary.width + offsetBoundary.x,  
+                        y: j * Boundary.height + offsetBoundary.y
+                    }
+                })
+            );
+        } else if (symbol === 5050) {
+            interactions.push(
+                new Boundary({
+                    position: {
+                        value: symbol, 
+                        x: k * Boundary.width + offsetBoundary.x,  
+                        y: j * Boundary.height + offsetBoundary.y
+                    }
+                })
+            );
+        } else if (symbol === 6060) {
+            interactions.push(
+                new Boundary({
+                    position: {
+                        value: symbol, 
+                        x: k * Boundary.width + offsetBoundary.x,  
+                        y: j * Boundary.height + offsetBoundary.y
+                    }
+                })
+            );
+        }
+        else if (symbol === 3030) {
+            interactions.push(
+                new Boundary({
+                    position: {
+                        value: symbol, 
+                        x: k * Boundary.width + offsetBoundary.x,  
+                        y: j * Boundary.height + offsetBoundary.y
+                    }
+                })
+            );
+        }
+    });
+});
+
+console.log(interactions);
+
 //orients the canvas to size
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 //import zoomed map, must be zoomed to 400% to look to scale 
 //importing sprite. i will have to take whatever the choose and make custom sprite sheets
 const spriteGuy = new Image()
-spriteGuy.src = './images/Dsprite.png'
-console.log(spriteGuy)
+
+
 //wait for sprite to load 
 
 const spriteGuyUP = new Image()
-spriteGuyUP.src = "./images/Dsprite.png"
+spriteGuyUP.src = "./images/barfUp.png"
 const spriteGuyDOWN = new Image()
-spriteGuyDOWN.src = "./images/Dsprite.png"
+spriteGuyDOWN.src = "./images/barfDown.png"
 const spriteGuyLEFT = new Image()
-spriteGuyLEFT.src = "./images/Dsprite.png"
+spriteGuyLEFT.src = "./images/barfLeft.png"
 const spriteGuyRIGHT = new Image()
-spriteGuyRIGHT.src = "./images/Dsprite.png"
+spriteGuyRIGHT.src = "./images/barfRight.png"
 // backgroundImage.onload = () => {
 //     //might need to change x and y to position where the screen starts
 
@@ -53,7 +135,7 @@ const newDude = new Sprite({
         x: canvas.width / 2 - 256 / 4 / 2, // these two should perfectly center this.image on canvas if he is 256/64,
         y: canvas.height / 2 - 64 / 2,
     },
-    image: spriteGuy,
+    image: spriteGuyDOWN,
     frames: {
         max: 4
     },
@@ -103,31 +185,72 @@ const testBoundary = new Boundary({
         y: 400
     }
 })
-const moveables = [background, ...boundaries, foreground]
+const moveables = [background, ...boundaries, foreground, ...interactions]
 //below function replaced my giant if statement that covered how the collision was narrowed down to the collision array objects positions on the map
 function boxCollision({ rectangle1, rectangle2 }) {
     return (
         rectangle1.position.x + rectangle1.width >= rectangle2.position.x && rectangle1.position.x <= rectangle2.position.x + rectangle2.width && rectangle1.position.y <= rectangle2.position.y + rectangle2.height && rectangle1.position.y + rectangle1.height >= rectangle2.position.y
     )
 }
+
+
 //animation loop
 function animate() {
-    window.requestAnimationFrame(animate) //infinite loop for the animation 
+    const gameanimation = window.requestAnimationFrame(animate) //infinite loop for the animation 
     background.draw()
     boundaries.forEach(boundary => {
         boundary.draw()
     })
-    newDude.draw()
+    interactions.forEach(interaction => {
+        interaction.draw();
+    });
+    
+ newDude.draw()
 foreground.draw()
     // if(spriteGuy.position.x + spriteGuy.width)
     //if statement for key actions to follow. !!!!! somewhere in movement there is a glitch that freezes right side obstacle interaction  
 
+
+
+    //key to triggering interaction 
+    if (keys.w.pressed || keys.a.pressed || keys.s.pressed || keys.d.pressed) {
+        for (let i = 0; i < interactions.length; i++) {
+            const interaction = interactions[i];
+            if (
+                boxCollision({
+                    rectangle1: newDude,
+                    rectangle2: interaction
+                })
+            ) {
+                if (interaction.position.value === 4750) {
+                
+                    console.log("Interacting with symbol 4750");
+                    
+                } else if (interaction.position.value === 2020) {
+                    console.log("Interacting with symbol 2020");
+                    
+                } else if (interaction.position.value === 5050) {
+                    console.log("Interacting with symbol 5050");
+                   
+                } else if (interaction.position.value === 6060) {
+                    console.log("Interacting with symbol 6060");
+                   
+                } else if (interaction.position.value === 3030) {
+                    console.log("Interacting with symbol 3030");
+                    
+                }
+                break;
+            }
+        }
+    }
+   
 
     let moving = false
     newDude.moving = false
     if (keys.w.pressed && lastkey === 'w') {
         moving = true
         newDude.moving = true
+        newDude.image = newDude.sprites.up
         for (let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i]
             if (
@@ -146,14 +269,15 @@ foreground.draw()
                 moving = false   //making a break and movement stop so the map quits moving and js quits detecting for other boundaries because it keeps giving a false reading 
                 break
             }
-        }
+        } 
         if (moving)
             moveables.forEach(moveable => {
                 moveable.position.y += 3
             })
-    }else if (keys.a.pressed && lastkey === 'a') {
+    } else if (keys.a.pressed && lastkey === 'a') {
         moving = true
         newDude.moving = true
+        newDude.image = newDude.sprites.left
         for (let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i]
             if (
@@ -177,6 +301,7 @@ foreground.draw()
     }else if (keys.s.pressed && lastkey === 's') {
         moving = true
         newDude.moving = true
+        newDude.image = newDude.sprites.down
         for (let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i]
             if (
@@ -200,6 +325,7 @@ foreground.draw()
     }else if (keys.d.pressed && lastkey === 'd') {
         moving = true
         newDude.moving = true
+        newDude.image = newDude.sprites.right
         for (let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i]
             if (
@@ -262,5 +388,5 @@ window.addEventListener('keyup', (e) => {
             keys.d.pressed = false
             break
     }
-    console.log(keys)
+    
 })
