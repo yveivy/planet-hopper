4bb759530ec4d725c59567c5f5af313c5c24aedc
Valais Girl <Yevette@Hal-71.local>
2023-05-17 10:18:23 -0500

    table seed input

--- seeds/wishlist-seeds.js
+++ seeds/wishlist-seeds.js
@@ -1,69 +1,47 @@
-const { wishlist } = require('../models');
+const { Wishlist } = require('../models');
 
 const wishlistData = [
 
+
   {
-    character_id:'',
-    item_id:'',
+    character_id: 7,
+    item_id: 3,
   },
   {
-    character_id:'',
-    item_id:'',
+    character_id: 4,
+    item_id: 4,
   },
   {
-    character_id:'',
-    item_id:'',
+    character_id: 1,
+    item_id: 5,
   },
   {
-    character_id:'',
-    item_id:'',
+    character_id:1,
+    item_id: 7,
   },
   {
-    character_id:'',
-    item_id:'',
+    character_id: 1,
+    item_id: 9,
   },
   {
-    character_id:'',
-    item_id:'',
+    character_id:2,
+    item_id: 10,
   },
   {
-    character_id:'',
-    item_id:'',
+    character_id:3,
+    item_id: 12,
   },
   {
-    character_id:'',
-    item_id:'',
+    character_id: 6,
+    item_id: 13,
   },
   {
-    character_id:'',
-    item_id:'',
+    character_id: 5,
+    item_id: 14,
   },
-  {
-    character_id:'',
-    item_id:'',
-  },
-  {
-    character_id:'',
-    item_id:'',
-  },
-  {
-    character_id:'',
-    item_id:'',
-  },
-  {
-    character_id:'',
-    item_id:'',
-  },
-  {
-    character_id:'',
-    item_id:'',
-  },
-  {
-    character_id:'',
-    item_id:'',
-  },
+  
 ];
 
-const seedWishlist = () => wishlist.bulkCreate(wishlistData);
+const seedWishlist = () => Wishlist.bulkCreate(wishlistData);
 
 module.exports = seedWishlist;
