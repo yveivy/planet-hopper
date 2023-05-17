4d6f55e95681b3498143181a25653f2bf0057fed
Valais Girl <Yevette@Hal-71.local>
2023-05-17 10:17:22 -0500

    git add seeds/seed.js git status ;

---
+++ seeds/seed.js
@@ -0,0 +1,24 @@
+const seedCharacters = require('./character-seeds');
+const seedItems = require('./item-seeds');
+const seedWishlist = require('./wishlist-seeds');
+const seedInventory = require('./inventory-seeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+  await seedCharacters();
+  console.log('\n----- CHARACTERS SEEDED -----\n');
+  await seedItems();
+  console.log('\n----- ITEMS SEEDED -----\n');
+  await seedWishlist();
+  console.log('\n----- WISHLIST SEEDED -----\n');
+  await seedInventory();
+  console.log('\n----- INVENTORY SEEDED -----\n');
+
+
+  process.exit(0);
+};
+module.export = seedAll();
+
