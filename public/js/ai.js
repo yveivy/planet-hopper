ab315acbae6bf5ca751498fffa226ef6143ce305
willcline613 <willrcline.atx@gmail.com>
2023-05-18 16:05:47 -0500

    trade dialogue functions work. ready to plugin the offerResponse.

--- public/js/ai.js
+++ public/js/ai.js
@@ -14,28 +14,35 @@ export async function fetchOpenAiApi(prompt) {
 
 
 export function createPromptForNpcResponseToTradeRequest(reqObj) {
-    var bio = "You are a 3rd generation farmer and a really gruff woman"
-    var role = "farmer"
-    var valuedItems = "tools"
-    var nonValuedItems = "art"
-    var itemNeededAndReasonWhy = "some duct tape to fix your shovel"
-    var inventory = "an art piece, a rake, and a broken shovel."
-    var userInventory = "duct tape, spaceship oil, and a rattlesnake skin."
-    var itemOfferedByUser = "rattlesnake skin"
-    var itemRequestedByUser = "art piece"
-    var offerDecision = "decline"
+    var bio = reqObj.bio
+    var role = reqObj.role
+    
+    var npcInventoryItems = reqObj.userInventoryItems
+    var userInventoryItems = reqObj.npcInventoryItems
+    var itemOfferedByUser = reqObj.itemOfferedByUser
+    var descriptionOfItemOfferedByUser = reqObj.descriptionOfItemOfferedByUser
+    var itemRequestedByUser = reqObj.itemRequestedByUser
+    var descriptionOfItemRequestedByUser = reqObj.descriptionOfItemRequestedByUser
+    var offerDecision = reqObj.offerDecision
 
 var prompt = 
 `You are an npc in a scifi themed trading-oriented RPG called planet hopper. User has crash landed on your planet, and he is trying to trade with you and other NPCs to eventually get the items he needs to get off the planet. The different NPCs value items differently depending on their personal preferences. Their personal preferences regarding various items will alter their willingness to accept the user's offers to trade.
 Here's a little information about you:
-Role-${role}
+Role- ${role}
 Bio- ${bio}.
-Inventory: You have ${inventory}
-The user has ${userInventory}
+Your inventory: ${npcInventoryItems}
+User inventory: ${userInventoryItems}
 
-The user offered to trade his ${itemOfferedByUser} for your ${itemRequestedByUser}, and you ${offerDecision}.
+The user offered to trade his: ${itemOfferedByUser}
+Description: ${descriptionOfItemOfferedByUser}
+
+He has requested your: ${itemRequestedByUser}
+Description: ${descriptionOfItemRequestedByUser}
+
+You ${offerDecision} his offer:
 Give me your verbal response in quotes to his offer. Don't respond with anything outside the quotes, or it will mess up the program
 `
+console.log("createPromptForNpcResponseToTradeRequest_______________", prompt)
     return prompt
 }
 
@@ -48,7 +55,7 @@ export function createPromptForNpcResponseToChat(reqObj) {
 var prompt = 
 `You are an npc in a scifi themed trading-oriented RPG called planet hopper. User has crash landed on your planet, and he is trying to trade with you and other NPCs to eventually get the items he needs to get off the planet. In the meantime, He is chatting you up.
 Here's a little information about you:
-Role-${role}
+Role- ${role}
 Bio- ${bio}
 
 ${chatHistory}
